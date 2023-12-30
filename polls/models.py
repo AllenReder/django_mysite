a7681b19d12462b6af48588f4f925afb496a03dd
Allen Yi <540809998@qq.com>
2023-12-30 18:02:37 +0800

    update functions and administration

--- polls/models.py
+++ polls/models.py
@@ -1,25 +1,31 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 import datetime
 
 
-
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    question_text = models.CharField(max_length=200, verbose_name="问题内容")
+    pub_date = models.DateTimeField("发布日期")
 
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="是否最近发布?",
+    )
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    
+        now = timezone.now()
+        return timezone.now() - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
