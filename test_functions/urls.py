d8a83b7febbdd6b98087e6c7a512023c7b19ecfa
Allen Yi <540809998@qq.com>
2024-01-01 14:04:07 +0800

    HTTP请求测试

--- test_functions/urls.py
+++ test_functions/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = "test_functions"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+    # path("", views.IndexView.as_view(), name="index"),
+    path("search_form/", views.search_form, name="search_form"),
+    path("search/", views.search, name="search"),
 ]
--- test_functions/views.py
+++ test_functions/views.py
@@ -5,23 +5,13 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 
-class IndexView(generic.TemplateView):
-    template_name = "test_functions/index.html"
 
-class IndexView(View):
-    def get(self, request, *args, **kwargs):
-        # 相应地处理GET请求
-        print('path info is : ', request.path_info)
-        print('method is : ', request.method)
-        print('querystring is : ', request.GET)
-        print('full path is :', request.get_full_path())
-        print('客户端IP is :', request.META['REMOTE_ADDR'])
+def search_form(request):
+    return render(request, 'test_functions/search_form.html')
 
-        total_sum = 0
-        for key, value in request.GET.items():
-            try:
-                total_sum += float(value)
-            except ValueError:
-                continue
-
-        return HttpResponse(f'test request, your sum is {total_sum}')
+def search(request):
+    if 'q' in request.GET:
+        message = '你正在查找: %r' % request.GET['q']
+    else:
+        message = '你查找了个寂寞.'
+    return HttpResponse(message)
